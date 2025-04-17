56f1a08ec6d750bbe2092898909d83d01326da6e
Lucas Pedro <botdanet38@gmail.com>
2025-04-17 19:12:51 -0300

    Remove .venv

---
+++ Exercícios.py
@@ -0,0 +1,103 @@
+#Exercicio 1
+# number = int(input('Digite um número: '))
+# if number > 0:
+#     print('é um número positvo')
+# elif number < 0:
+#     print('é um número negativo')
+# else:
+#     print('è igual a zero')
+
+
+#Exercicio 2
+# a = int(input('Digite sua nota: '))
+# b = int(input('Digite sua nota: '))
+# c = int(input('Digite sua nota: '))
+#
+# resultado = a + b + c
+# resultado2 = resultado / 3
+# if resultado2 >= 7:
+#     print('Aprovado')
+# elif resultado2 >= 5:
+#     print('Recuperação')
+# else:
+#     print('Reprovado')
+
+
+#exercicio 3
+# a = int(input('informe sua idade: '))
+#
+# idade = 'Você é maior' if a >= 18 else 'Você é menor'
+# print(idade)
+
+#exercicio 4
+# a = (200, 300, 400)
+#
+# maior = a[0]
+# if a [1] > maior:
+#     maior = a[1]
+#
+#     if a [2] > maior:
+#         maior = a[2]
+#
+#         print(maior)
+
+
+#exercicio 5
+# a = int(input('Digite um número: '))
+# if a > 10 and a < 50:
+#     print('O número está entre o intervalo')
+# else:
+#     print('Não está dentro do intervalo')
+
+
+#exercicio 6
+#lista = ['Mario', 'Marcelo', 'Martinelli']
+
+#listastring = len(lista[0])
+
+#if len(lista[1]) > listastring:
+#    listastring = len(lista[1])
+#if len(lista[2]) > listastring:
+#   listastring = len(lista[2])
+
+#print(listastring)
+
+
+#exercício 7
+# dia = 1
+#
+# match dia:
+#     case 1:
+#         nome_dia = 'Domingo'
+#     case 2:
+#         nome_dia = 'segunda'
+#     case 3:
+#         nome_dia = 'Terça'
+#     case 4:
+#         nome_dia = 'Quarta'
+#     case 5:
+#         nome_dia = 'Quinta'
+#     case 6:
+#         nome_dia = 'Sexta'
+#     case 7:
+#         nome_dia = 'Sábado'
+#     case _:
+#         nome_dia = 'Oppção inválida'
+#
+# print(nome_dia)
+
+
+#exercicio 8
+# a = (300, 300, 300)
+#
+#
+# if (a[0]) == (a[1]) == (a[2]):
+#     print('Todos iguais')
+# elif (a[0]) != (a[1]) != (a[2]):
+#     print('Todos diferentes')
+# else:
+#     print('Dois valores iguais')
+#      print(a)
+
+
+#exercicio 9
